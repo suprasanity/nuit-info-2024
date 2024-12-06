f92181286c260eae79da8308632d5c2e3fdafc5d
Suprasanity <yann.jeanmaire@gmail.com>
2024-12-06 04:52:04 +0100

    qrcode

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -1,30 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Smartphone } from 'lucide-react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
-import '../style/QRCode.css';
+
+const isMobile = () => {
+    return /Mobi|Android/i.test(navigator.userAgent);
+};
 
 export const QRCode = () => {
-    const [isClosed, setIsClosed] = useState(false);
-
-    const handleClose = () => {
-        setIsClosed(true);
-    };
+    if (isMobile()) {
+        return null;
+    }
 
     return (
-        <div className={`fixed bottom-4 right-4 md:bottom-8 md:right-8 ${isClosed ? 'hidden' : ''}`}>
+        <div className="fixed bottom-4 right-4 md:bottom-8 md:right-8">
             <div className="bg-white p-4 rounded-lg shadow-lg">
-                <div className="icon-container flex justify-between">
-                    <Smartphone className="w-6 h-6 text-blue-600 mb-2"/>
-                    <FontAwesomeIcon
-                        icon={faXmark}
-                        className="text-red-600 cursor-pointer"
-                        onClick={handleClose}
-                    />
-                </div>
+                <Smartphone className="w-6 h-6 text-blue-600 mb-2" />
                 <p className="text-sm text-gray-600">Scannez pour voir sur mobile</p>
                 <div className="w-24 h-24 bg-gray-200 mt-2 rounded">
-                    <img src="/qrcode.png" alt="QR Code" className="w-24 h-24 mt-2 rounded"/>
+                    <img src="/qrcode.png" alt="QR Code" className="w-24 h-24 mt-2 rounded" />
                 </div>
             </div>
         </div>
